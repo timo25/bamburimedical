cd84b1f2675494d3872fe764bac1be0c5d88d9eb
timo25 <timothyndungo@gmail.com>
2022-02-09 05:00:44 +0300

    Initial commit

--- myHospital/settings.py
+++ myHospital/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'bamburiafyacentre.herokuapp.com']
+ALLOWED_HOSTS = [ 'bamburiafyacentre.herokuapp.com']
 
 
 # Application definition
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
